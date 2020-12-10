268b687b4d84b44c7af26cde7dbe16396e334680
hisakot <sakigake.apple@gmail.com>
2020-12-10 10:59:15 +0900

    color and dataset change

--- fillpoly.py
+++ fillpoly.py
@@ -8,7 +8,8 @@ import re
 
 import torch
 
-DATA_CSV = "../data/tool/annotations20201202.csv"
+# DATA_CSV = "../data/tool/annotations20201202.csv"
+DATA_CSV = "../data/tool2/annotation_data_2.csv"
 DATASET_CACHE = "./dataset_cache"
 MASK_COLOR = [(92, 92, 205), (193, 182, 255), (71, 99, 255), (0, 215, 255), (113, 179, 60), (237, 149, 100), (128, 0, 128), (30, 105, 210)]
 
@@ -33,11 +34,11 @@ for i in range(len(csv_data.values) - 1):
     for j, x in enumerate(region_shape_attributes["all_points_x"]):
         xy_list.append((x, region_shape_attributes["all_points_y"][j]))
     vertex = np.array(xy_list)
-#     b = random.randrange(256)
-#     g = random.randrange(256)
-#     r = random.randrange(256)
-#     cv2.fillPoly(img, [vertex], (b, g, r))
-    cv2.fillPoly(img, [vertex], MASK_COLOR[tool_label - 1])
+    b = random.randrange(256)
+    g = random.randrange(256)
+    r = random.randrange(256)
+    cv2.fillPoly(img, [vertex], (b, g, r))
+#     cv2.fillPoly(img, [vertex], MASK_COLOR[tool_label - 1])
     box_list.append([min(region_shape_attributes["all_points_x"]),
                    min(region_shape_attributes["all_points_y"]),
                    max(region_shape_attributes["all_points_x"]),
@@ -45,7 +46,7 @@ for i in range(len(csv_data.values) - 1):
     tool_list.append(tool_label)
 
     if csv_data.values[i][1] - 1 == csv_data.values[i][2]:
-        cv2.imwrite("../data/tool/masks/" + csv_data.values[i][0], img)
+        cv2.imwrite("../data/tool2/masks/" + csv_data.values[i][0], img)
         dataset.append({"filename" : csv_data.values[i][0],
                         "box_list" : box_list,
                         "tool_label" : tool_list,})
